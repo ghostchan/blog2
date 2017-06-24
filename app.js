90003030dea1ee80d5f1c63094fced1a534ab978
jinn <jinnchan@qq.com>
2017-06-24 21:39:51 +0800

    4-静态文件托管

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var swig = require('swig');
 var app = express();
 
+app.use('/public',express.static(__dirname + '/public'));
 app.engine('html',swig.renderFile);
 app.set('views','./views');
 app.set('view engine','html');
@@ -10,4 +11,9 @@ app.get('/',function (req,res,next) {
     // res.send('<h1>欢迎光临我的博客！</h1>');
     res.render('index');
 });
+
+/*app.get('/main.css',function (req,res,next) {
+    res.setHeader('content-type','text/css');
+    res.send("body {background:green;}");
+});*/
 app.listen(8081);
