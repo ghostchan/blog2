d59973d0a22d27492727e83d9171713598cc4c41
jinn <jinnchan@qq.com>
2017-06-24 21:01:16 +0800

    3-模板引擎的配置和使用

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var swig = require('swig');
 var app = express();
 
+app.engine('html',swig.renderFile);
+app.set('views','./views');
+app.set('view engine','html');
+swig.setDefaults({cache:false});
 app.get('/',function (req,res,next) {
-    res.send('<h1>欢迎光临我的博客！</h1>');
+    // res.send('<h1>欢迎光临我的博客！</h1>');
+    res.render('index');
 });
 app.listen(8081);
