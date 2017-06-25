af09bddec30d227085891844dacd3f0cac742db2
jinn <jinnchan@qq.com>
2017-06-25 17:18:57 +0800

    5

--- app.js
+++ app.js
@@ -7,13 +7,8 @@ app.engine('html',swig.renderFile);
 app.set('views','./views');
 app.set('view engine','html');
 swig.setDefaults({cache:false});
-app.get('/',function (req,res,next) {
-    // res.send('<h1>欢迎光临我的博客！</h1>');
-    res.render('index');
-});
 
-/*app.get('/main.css',function (req,res,next) {
-    res.setHeader('content-type','text/css');
-    res.send("body {background:green;}");
-});*/
+app.use('/admin',require('./routers/admin'));
+app.use('/api',require('./routers/api'));
+app.use('/',require('./routers/main'));
 app.listen(8081);
